42871bd14b77f532e4aff1c6dd70f7d7d865bede
archana-ragam <66559726+archana-ragam@users.noreply.github.com>
2020-08-12 21:32:06 +0530

    Create Progms.py

---
+++ Progms.py
@@ -0,0 +1,15 @@
+/* area of circle with radius*/
+Pie=3.14
+r=int(input("enter the radius;"))
+area=pie*r*r
+Print("area of circle:",area)
+
+
+
+/*extention file*/
+D={"add.py":"python",
+    "Add.java":"java",
+     "Add.c":"c"
+}
+File=str(input("enter the file with extention;"))
+print("the extension of file:",d.get(File,-1))
